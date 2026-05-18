4110a2f502a2eee870e5a438d102a3da7bea5743
Rafa Jimenez <rafajima2002@gmail.com>
2026-05-18 16:51:53 +0000

    capas terminadas

--- models/marts/core/fct__ventas.sql
+++ models/marts/core/fct__ventas.sql
@@ -3,10 +3,18 @@
 WITH fct_ventas AS (
     SELECT
         v.*,
-        e.* exclude(id_estacion),
+        eo.codigo_estacion AS codigo_estacion_origen,
+        eo.nombre_estacion AS estacion_origen,
+        eo.municipio AS municipio_origen,
+        eo.provincia AS provincia_origen,
+        ed.codigo_estacion AS codigo_estacion_destino,
+        ed.nombre_estacion AS estacion_destino,
+        ed.municipio AS municipio_destino,
+        ed.provincia AS provincia_destino,
         p.* exclude(id_pasajero)
     FROM {{ ref('stg__ventas') }} v
-    LEFT JOIN {{ ref('dim__estaciones') }} e ON v.id_estacion_origen = e.id_estacion
+    LEFT JOIN {{ ref('dim__estaciones') }} eo ON v.id_estacion_origen = eo.id_estacion
+    LEFT JOIN {{ ref('dim__estaciones') }} ed ON v.id_estacion_destino = ed.id_estacion
     LEFT JOIN {{ ref('dim__pasajeros') }} p ON v.id_pasajero = p.id_pasajero
     LEFT JOIN {{ ref('dim__tiempo') }} dt ON v.fecha_viaje = dt.fecha_dia
 )
