31acb7c31bfd90aeb24d01e069415a0d5cbf8b07
Rafa Jimenez <rafajima2002@gmail.com>
2026-05-20 10:55:08 +0000

    oh

--- models/marts/core/dim__tiempo.sql
+++ models/marts/core/dim__tiempo.sql
@@ -40,7 +40,7 @@ final AS (
         END AS mes,
 
         TO_CHAR(date_day, 'YYYY-MM') AS anio_mes,
-        (YEAR(date_day) * 100 + MONTH(date_day)) AS anio_mes_orden
+        (YEAR(date_day) * 100 + MONTH(date_day)) AS anio_mes_orden,
 
         DAY(date_day) AS dia,
 
